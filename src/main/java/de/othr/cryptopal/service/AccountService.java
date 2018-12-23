ccb63e2fe749d96ac9d0549f2ea3ce8c1297b2e1
simonoppowa <simon.oppowa@st.oth-regensburg.de>
2018-12-23 18:23:18 +0100

    added register errors

--- src/main/java/de/othr/cryptopal/service/AccountService.java
+++ src/main/java/de/othr/cryptopal/service/AccountService.java
@@ -2,17 +2,36 @@ package de.othr.cryptopal.service;
 
 import de.othr.cryptopal.entity.Account;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.*;
 import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
-
+@ApplicationScoped
 public class AccountService implements Serializable {
 
     @PersistenceContext
     private EntityManager em;
+
+    //TODO Remove
+    @PostConstruct
+    @Transactional
+    public void createDummies() {
+        Account account1 = new Account("max.mustermann@gmx.de", "123");
+        Account account2 = new Account("manfred.mueller@gmail.com", "321");
+        Account account3 = new Account("maria.meister@freenet.de", "test");
+
+        try {
+            em.persist(account1);
+            em.persist(account2);
+            em.persist(account3);
+        } catch(PersistenceException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Dummies created");
+    }
 
     @Transactional
     public Account createNewAccount(@NotNull Account account, String confirmPassword) {
@@ -30,5 +49,23 @@ public class AccountService implements Serializable {
         }
 
         return account;
+    }
+
+    public boolean checkIfAccountAlreadyExists(String email){
+
+        TypedQuery<Account> typedQuery = em.createNamedQuery(Account.FINDBYEMAIL, Account.class);
+        typedQuery.setParameter("email", email);
+
+        try {
+            typedQuery.getSingleResult();
+            return true;
+        } catch (NoResultException ex) {
+            return false;
+        }
+    }
+
+    // TODO
+    private void checkIfAccountNameIsBanned() {
+
     }
 }
--- src/main/java/de/othr/cryptopal/ui/model/AccountModel.java
+++ src/main/java/de/othr/cryptopal/ui/model/AccountModel.java
@@ -17,26 +17,34 @@ public class AccountModel implements Serializable {
     @Inject
     private AccountService accountService;
 
-
     @Inject
     private CredentialsModel credentials;
 
     private Account loggedInAccount;
 
-    public void doRegister() {
+    public String doRegister() {
         System.out.println("Register called with " + credentials.getEmail() + " " + credentials.getPassword() + " "
                 + credentials.getConfirmPassword());
-        // TODO Add warning for null or empty
-        // TODO hardcoded text
 
-        //
-        //
+        // Check if email field is filled out
+        if(credentials.getEmail() == null || credentials.getEmail().equals("")) {
+            addWarningMessage("fill_out_email_field");
+        }
+        // Check if password field is filled out
+        if(credentials.getPassword() == null || credentials.getPassword().equals("")) {
+            addWarningMessage("fill_out_password_field");
+        }
 
+        // Check if email is already used
+        if(accountService.checkIfAccountAlreadyExists(credentials.getEmail())) {
+            addWarningMessage("login_already_exists");
+            return null;
+        }
+
+        // Check if passwords match
         if(!credentials.getPassword().equals(credentials.getConfirmPassword())) {
-            System.out.println("Passwords don't match");
-            FacesContext.getCurrentInstance().addMessage("myform:email",
-                    new FacesMessage("Passwords don't match", "Passwords don't match"));
-            return;
+            addWarningMessage("passwords_don't_match");
+            return null;
         }
 
         Account newAccount = new Account(credentials.getEmail(), credentials.getPassword());
@@ -47,7 +55,20 @@ public class AccountModel implements Serializable {
             System.out.println("New account created: " + loggedInAccount.getEmail() + " " + loggedInAccount.getPassword());
         } else {
             System.out.println("Error while creating account");
+            addWarningMessage("critical_account_creation_error");
         }
 
+        return "createaccount.faces";
+    }
+
+    private void addWarningMessage(String message, Object... args) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage("registerform", new FacesMessage(FacesMessage.SEVERITY_WARN,
+                getMessage(context, message, args), null));
+    }
+
+    private String getMessage(FacesContext facesContext, String msgKey, Object... args) {
+        // TODO load message from properties
+        return "Error 404: " + msgKey;
     }
 }
